370eeed7c41cf155a1285aaecc7fa628b1bf7cb3
bediako95 <p.bediako1999@gmail.com>
2022-08-26 18:00:52 +0000

    update: authentication: add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_135524) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_162837) do
   create_table "orders", force: :cascade do |t|
     t.string "name"
     t.string "phone"
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_135524) do
     t.string "order_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
